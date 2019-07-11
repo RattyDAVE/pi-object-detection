a90f00e4aafdbf2412ce3af544401a1702dbeadf
RattyDAVE <dave@pucknell.co.uk>
2019-07-11 15:13:50 +0100

    Update Object_detection_picamera.py

--- Object_detection_picamera.py
+++ Object_detection_picamera.py
@@ -1,10 +1,7 @@
 import os
 import cv2
 import numpy as np
-from picamera.array import PiRGBArray
-from picamera import PiCamera
 import tensorflow as tf
-import argparse
 import sys
 
 import configparser
@@ -22,14 +19,6 @@ PATH_TO_LABELS = os.path.join(CWD_PATH,'data',config['DEFAULT']['PATH_TO_LABELS'
 NUM_CLASSES = int(config['DEFAULT']['NUM_CLASSES'])
 
 PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,'frozen_inference_graph.pb')
-
-camera_type = 'picamera'
-parser = argparse.ArgumentParser()
-parser.add_argument('--usbcam', help='Use a USB webcam instead of picamera',
-                    action='store_true')
-args = parser.parse_args()
-if args.usbcam:
-    camera_type = 'usb'
 
 sys.path.append('..')
 
@@ -60,43 +49,11 @@ frame_rate_calc = 1
 freq = cv2.getTickFrequency()
 font = cv2.FONT_HERSHEY_SIMPLEX
 
-if camera_type == 'picamera':
-    camera = PiCamera()
-    camera.resolution = (IM_WIDTH,IM_HEIGHT)
-    camera.framerate = 10
-    rawCapture = PiRGBArray(camera, size=(IM_WIDTH,IM_HEIGHT))
-    rawCapture.truncate(0)
+camera = cv2.VideoCapture(0)
+ret = camera.set(3,IM_WIDTH)
+ret = camera.set(4,IM_HEIGHT)
 
-    for frame1 in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):
-
-        t1 = cv2.getTickCount()
-        frame = np.copy(frame1.array)
-        frame.setflags(write=1)
-        frame_expanded = np.expand_dims(frame, axis=0)
-        (boxes, scores, classes, num) = sess.run(
-            [detection_boxes, detection_scores, detection_classes, num_detections],
-            feed_dict={image_tensor: frame_expanded})
-        vis_util.visualize_boxes_and_labels_on_image_array(frame,np.squeeze(boxes),np.squeeze(classes).astype(np.int32),np.squeeze(scores),category_index,use_normalized_coordinates=True,line_thickness=8,min_score_thresh=0.40)
-
-        cv2.putText(frame,"FPS: {0:.2f}".format(frame_rate_calc),(30,50),font,1,(255,255,0),2,cv2.LINE_AA)
-        cv2.imshow('Object detector', frame)
-
-        t2 = cv2.getTickCount()
-        time1 = (t2-t1)/freq
-        frame_rate_calc = 1/time1
-        if cv2.waitKey(1) == ord('q'):
-            break
-
-        rawCapture.truncate(0)
-
-    camera.close()
-
-elif camera_type == 'usb':
-    camera = cv2.VideoCapture(0)
-    ret = camera.set(3,IM_WIDTH)
-    ret = camera.set(4,IM_HEIGHT)
-
-    while(True):
+while(True):
 
         t1 = cv2.getTickCount()
         ret, frame = camera.read()
@@ -111,9 +68,8 @@ elif camera_type == 'usb':
         frame_rate_calc = 1/time1
 
         # Press 'q' to quit
-        if cv2.waitKey(1) == ord('q'):
-            break
+        #if cv2.waitKey(1) == ord('q'):
+        #    break
 
-    camera.release()
-
+camera.release()
 cv2.destroyAllWindows()
