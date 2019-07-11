6df1c439000c8cf86f4d6f653305cfad8af8a4e6
RattyDAVE <dave@pucknell.co.uk>
2019-07-11 07:26:47 +0100

    Update Object_detection_picamera.py

--- Object_detection_picamera.py
+++ Object_detection_picamera.py
@@ -9,11 +9,6 @@ import sys
 
 import config
 
-IM_WIDTH = 1280
-IM_HEIGHT = 720
-#IM_WIDTH = 640    Use smaller resolution for
-#IM_HEIGHT = 480   slightly faster framerate
-
 MODEL_NAME = 'ssdlite_mobilenet_v2_coco_2018_05_09'
 CWD_PATH = os.getcwd()
 PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,'frozen_inference_graph.pb')
