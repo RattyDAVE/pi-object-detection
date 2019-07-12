e4c0dca9ee2a930c7bc8ca0ae777cd2dcf07832f
RattyDAVE <dave@pucknell.co.uk>
2019-07-12 13:06:14 +0100

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo pip3 uninstall $(pip3 show tensorflow | grep Requires | sed 's/Requires: //g; s/,//g')
-sudo apt purge -y libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev qt4-dev-tools python3-opencv python3-tk libatlas-base-dev python3-protobuf protobuf-compiler python3-matplotlib python3-pil python3-lxml
+sudo apt purge -y libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev qt4-dev-tools python3-opencv python3-tk libatlas-base-dev python3-protobuf protobuf-compiler python3-matplotlib python3-pil python3-lxml python3-grpcio python3-keras-preprocessing python3-protobuf python3-keras-applications python3-wrapt python3-termcolor python3-astor python3-h5py python3-markdown
 rm -rf ~/tensorflow1
 rm -rf ~/Tensorflow-bin
 sed -i '/tensorflow1/d' ~/.bashrc
