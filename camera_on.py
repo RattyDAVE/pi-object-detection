470b880ba8ddbf270e341c65f3591ed0fab69468
RattyDAVE <dave@pucknell.co.uk>
2019-07-14 21:18:29 +0100

    Update camera_on.py

--- camera_on.py
+++ camera_on.py
@@ -53,17 +53,44 @@ camera = cv2.VideoCapture(0)
 ret = camera.set(3,IM_WIDTH)
 ret = camera.set(4,IM_HEIGHT)
 
+#Loop it!
 while(True):
 
+        #FPS start counter
         t1 = cv2.getTickCount()
+        
+        #Get frame from camera
         ret, frame = camera.read()
+        
+        #Resize frame
         frame_expanded = np.expand_dims(frame, axis=0)
-        (boxes, scores, classes, num) = sess.run([detection_boxes, detection_scores, detection_classes, num_detections],feed_dict={image_tensor: frame_expanded})
-        vis_util.visualize_boxes_and_labels_on_image_array(frame,np.squeeze(boxes),np.squeeze(classes).astype(np.int32),np.squeeze(scores),category_index,use_normalized_coordinates=True,line_thickness=8,min_score_thresh=0.20)
+        
+        # Actual detection.
+        (boxes, scores, classes, num) = sess.run(
+            [detection_boxes, detection_scores, detection_classes, num_detections],
+            feed_dict={image_tensor: frame_expanded})
+
+        # Visualization of the results of a detection.
+        vis_util.visualize_boxes_and_labels_on_image_array(
+            frame,
+            np.squeeze(boxes),
+            np.squeeze(classes).astype(np.int32),
+            np.squeeze(scores),
+            category_index,
+            use_normalized_coordinates=True,
+            line_thickness=8,
+            min_score_thresh=0.20)
+
+        #Put the FPS over on the frame
         cv2.putText(frame,"FPS: {0:.2f}".format(frame_rate_calc),(30,50),font,1,(255,255,0),2,cv2.LINE_AA)
+        
+        #Show the Frame
         cv2.imshow('Object detector', frame)
 
+        #FPS End counter
         t2 = cv2.getTickCount()
+        
+        #Work out FPS
         time1 = (t2-t1)/freq
         frame_rate_calc = 1/time1
 
