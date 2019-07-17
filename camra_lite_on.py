8e78c4b4203ec5b549bcf7157ec2935c714734e7
RattyDAVE <dave@pucknell.co.uk>
2019-07-17 16:20:05 +0100

    Update camra_lite_on.py

--- camra_lite_on.py
+++ camra_lite_on.py
@@ -4,23 +4,9 @@ import numpy as np
 import tensorflow as tf
 import sys
 
-import configparser
 
-config = configparser.ConfigParser()
-config.read('obj-config.ini')
-
-IM_WIDTH = int(config['DEFAULT']['IM_WIDTH'])
-IM_HEIGHT= int(config['DEFAULT']['IM_HEIGHT'])
-
-MODEL_NAME = config['DEFAULT']['MODEL_NAME']
-
-CWD_PATH = os.getcwd()
-PATH_TO_LABELS = os.path.join(CWD_PATH,'data',config['DEFAULT']['PATH_TO_LABELS'])
-NUM_CLASSES = int(config['DEFAULT']['NUM_CLASSES'])
-
-PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,'frozen_inference_graph.pb')
-
-sys.path.append('..')
+PATH_TO_LABELS = 'labels'
+TF_MODEL='detect.tflite'
 
 frame_rate_calc = 1
 freq = cv2.getTickFrequency()
@@ -38,22 +24,15 @@ label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
-detection_graph = tf.Graph()
+interpreter = tf.contrib.lite.Interpreter(model_path=TF_MODEL)
 
-with detection_graph.as_default():
-    od_graph_def = tf.compat.v1.GraphDef()
-    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
-        serialized_graph = fid.read()
-        od_graph_def.ParseFromString(serialized_graph)
-        tf.import_graph_def(od_graph_def, name='')
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
-    sess = tf.compat.v1.Session(graph=detection_graph)
-
-image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
-detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
-num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+boxes = interpreter.get_tensor(output_details[0]['index'])
+classes = interpreter.get_tensor(output_details[1]['index'])
+sores = interpreter.get_tensor(output_details[2]['index'])
+num = int(interpreter.get_tensor(output_details[3]['index'])[0])
 
 #Loop it!
 while(True):
@@ -68,9 +47,12 @@ while(True):
         frame_expanded = np.expand_dims(frame, axis=0)
         
         # Actual detection.
-        (boxes, scores, classes, num) = sess.run(
-            [detection_boxes, detection_scores, detection_classes, num_detections],
-            feed_dict={image_tensor: frame_expanded})
+        #(boxes, scores, classes, num) = sess.run(
+        #    [detection_boxes, detection_scores, detection_classes, num_detections],
+        #    feed_dict={image_tensor: frame_expanded})
+        
+        interpreter.set_tensor(input_details[0]['index'], frame_expanded)
+        interpreter.invoke()
 
         # Visualization of the results of a detection.
         vis_util.visualize_boxes_and_labels_on_image_array(
@@ -80,7 +62,7 @@ while(True):
             np.squeeze(scores),
             category_index,
             use_normalized_coordinates=True,
-            line_thickness=8,
+            line_thickness=2,
             min_score_thresh=0.20)
 
         #Put the FPS over on the frame
