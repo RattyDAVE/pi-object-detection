6f5ac1533a2a58fa1fdc9b1ce84ec6a0080c2755
RattyDAVE <dave@pucknell.co.uk>
2019-07-18 14:14:00 +0100

    Update camera_lite_on.py

--- camera_lite_on.py
+++ camera_lite_on.py
@@ -1,79 +1,69 @@
-import os
-import cv2
-import numpy as np
-import tensorflow as tf
-import sys
-
 #from https://www.tensorflow.org/lite/guide/hosted_models
 #wget http://download.tensorflow.org/models/mobilenet_v1_2018_08_02/mobilenet_v1_0.25_128_quant.tgz
-PATH_TO_LABELS = 'data/mscoco_label_map.pbtxt'
-TF_MODEL='mobilenet_v1_0.25_128_quant.tflite'
-#TF_MODEL='mobilenet_v2_1.0_224.tflite'
 
-NUM_CLASSES = 90
+import tensorflow as tf
+import cv2
+import numpy as np
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+import configparser
+
+config = configparser.ConfigParser()
+config.read('camera_lite_on.ini')
+
+IM_WIDTH = int(config['DEFAULT']['IM_WIDTH'])
+IM_HEIGHT= int(config['DEFAULT']['IM_HEIGHT'])
+MODEL_NAME = config['DEFAULT']['MODEL_NAME']
+
+CWD_PATH = os.getcwd()
+PATH_TO_LABELS = os.path.join(CWD_PATH,'data',config['DEFAULT']['PATH_TO_LABELS'])
+NUM_CLASSES = int(config['DEFAULT']['NUM_CLASSES'])
+
+#a = tf.lite.Interpreter('mobilenet_v1_1.0_224.tflite')
+#a = tf.lite.Interpreter('mobilenet_v2_1.0_224.tflite')
+a = tf.lite.Interpreter(MODEL_NAME)
+
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+a.allocate_tensors()
+input_details = a.get_input_details()
+output_details = a.get_output_details()
 
 frame_rate_calc = 1
 freq = cv2.getTickFrequency()
 font = cv2.FONT_HERSHEY_SIMPLEX
 
 camera = cv2.VideoCapture(0)
-ret = camera.set(3,640)
-ret = camera.set(4,480)
+ret = camera.set(3,IM_WIDTH)
+ret = camera.set(4,IM_HEIGHT)
 
-from utils import label_map_util
-from utils import visualization_utils as vis_util
-
-#from object_detection.utils import label_map_util
-#from object_detection.utils import visualization_utils as vis_util
-
-label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
-category_index = label_map_util.create_category_index(categories)
-
-interpreter = tf.lite.Interpreter(model_path=TF_MODEL)
-
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
-
-# check the type of the input tensor
-floating_model = False
-if input_details[0]['dtype'] == np.float32:
-  floating_model = True
-
-height = input_details[0]['shape'][1]
-width = input_details[0]['shape'][2] 
-
-print("height=",height)
-print("width=",width)
-
-#Loop it!
 while(True):
 
-        #FPS start counter
-        t1 = cv2.getTickCount()
+    #FPS start counter
+    t1 = cv2.getTickCount()
         
-        #Get frame from camera
-        ret, frame = camera.read()
-        
-        #Resize frame
-        frame2 = cv2.resize(frame, (height,width))
-        frame_expanded = np.expand_dims(frame2, axis=0)
+    #Get frame from camera
+    ret, frame = camera.read()
 
-        if floating_model:        
-            frame_expanded = (np.float32(frame_expanded) - 127.5) / 127.5
+    #frame = cv2.imread("test.jpg")
+    frame_expanded = cv2.resize(frame, (300,300))
+    frame_expanded = np.expand_dims(frame_expanded, axis=0)
+    #frame_expanded = (np.float32(frame_expanded) - 127.5) / 127.5
+    frame_expanded = np.uint8(np.float32(frame_expanded))
 
-        #Actual detection.     
-        interpreter.set_tensor(input_details[0]['index'], frame_expanded)
-        interpreter.invoke()
+    #Actual detection.
+    a.set_tensor(input_details[0]['index'], frame_expanded)
+    a.invoke()
 
-        boxes = interpreter.get_tensor(output_details[0]['index'])
-        classes = interpreter.get_tensor(output_details[1]['index'])
-        scores = interpreter.get_tensor(output_details[2]['index'])
-        num = int(interpreter.get_tensor(output_details[3]['index'])[0])
-
-
-        #Visualization of the results of a detection.
-        vis_util.visualize_boxes_and_labels_on_image_array(
+    boxes = a.get_tensor(output_details[0]['index']) 
+    classes = a.get_tensor(output_details[1]['index'])
+    scores = a.get_tensor(output_details[2]['index'])
+    num = a.get_tensor(output_details[3]['index'])
+  
+    #Visualization of the results of a detection.
+    vis_util.visualize_boxes_and_labels_on_image_array(
             frame,
             np.squeeze(boxes),
             np.squeeze(classes).astype(np.int32),
@@ -83,22 +73,20 @@ while(True):
             line_thickness=2,
             min_score_thresh=0.20)
 
-        #Put the FPS over on the frame
-        cv2.putText(frame,"FPS: {0:.2f}".format(frame_rate_calc),(30,50),font,1,(255,255,0),2,cv2.LINE_AA)
+    #Put the FPS over on the frame
+    cv2.putText(frame,"FPS: {0:.2f}".format(frame_rate_calc),(30,50),font,1,(255,255,0),2,cv2.LINE_AA)
         
-        #Show the Frame
-        cv2.imshow('Object detector', frame)
+    #Show the Frame
+    cv2.imshow('Object detector', frame)
 
-        #FPS End counter
-        t2 = cv2.getTickCount()
+    #FPS End counter
+    t2 = cv2.getTickCount()
         
-        #Work out FPS
-        time1 = (t2-t1)/freq
-        frame_rate_calc = 1/time1
+    #Work out FPS
+    time1 = (t2-t1)/freq
+    frame_rate_calc = 1/time1
 
-        # Press 'q' to quit
-        if cv2.waitKey(1) == ord('q'):
-            break
+    cv2.waitKey(1)
 
 camera.release()
 cv2.destroyAllWindows()
