3c8c55b07f6392237bc3b8dafd014f4e425c42e7
RattyDAVE <dave@pucknell.co.uk>
2019-07-19 07:47:46 +0100

    Update camera_lite_on.py

--- camera_lite_on.py
+++ camera_lite_on.py
@@ -1,5 +1,6 @@
 #from https://www.tensorflow.org/lite/guide/hosted_models
 #wget http://download.tensorflow.org/models/mobilenet_v1_2018_08_02/mobilenet_v1_0.25_128_quant.tgz
+#https://coral.withgoogle.com/models/
 
 import tensorflow as tf
 import cv2
@@ -12,25 +13,56 @@ config = configparser.ConfigParser()
 config.read('camera_lite_on.ini')
 
 IM_WIDTH = int(config['DEFAULT']['IM_WIDTH'])
+print(IM_WIDTH)
 IM_HEIGHT= int(config['DEFAULT']['IM_HEIGHT'])
+print(IM_HEIGHT)
 MODEL_NAME = config['DEFAULT']['MODEL_NAME']
+print(MODEL_NAME)
 
-CWD_PATH = os.getcwd()
-PATH_TO_LABELS = os.path.join(CWD_PATH,'data',config['DEFAULT']['PATH_TO_LABELS'])
+PATH_TO_LABELS = config['DEFAULT']['PATH_TO_LABELS']
+print(PATH_TO_LABELS)
 NUM_CLASSES = int(config['DEFAULT']['NUM_CLASSES'])
+print(NUM_CLASSES)
 
-#a = tf.lite.Interpreter('mobilenet_v1_1.0_224.tflite')
-#a = tf.lite.Interpreter('mobilenet_v2_1.0_224.tflite')
+file = open(PATH_TO_LABELS, 'r')
+line = file.readline()
+end = '\n'
+s = ' '
+ID = 1 
+#for name in file.readline()
+for line in file:
+#while line:
+    out = ''
+    out += 'item' + s + '{' + end
+    out += s*2 + 'name:' + ' ' + '\"' + line.strip('\n') + '\"' + end
+    out += s*2 + 'id:' + ' ' + (str(ID)) + end
+    out += '}' + end
+    
+    with open(PATH_TO_LABELS+".conv", 'a') as f:
+        f.write(out)
+    print(out)    
+    ID = ID + 1
+
+PATH_TO_LABELS=PATH_TO_LABELS+".conv"
+
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+#print("")
+#print(category_index)
+#print("")
+
+
+
 a = tf.lite.Interpreter(MODEL_NAME)
-
-labels = load_labels(args.label_file)
-#label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-#categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
-#category_index = label_map_util.create_category_index(categories)
-
 a.allocate_tensors()
 input_details = a.get_input_details()
 output_details = a.get_output_details()
+
+print ("Out")
+print (output_details)
+
 
 frame_rate_calc = 1
 freq = cv2.getTickFrequency()
@@ -61,7 +93,7 @@ while(True):
     boxes = a.get_tensor(output_details[0]['index']) 
     classes = a.get_tensor(output_details[1]['index'])
     scores = a.get_tensor(output_details[2]['index'])
-    num = a.get_tensor(output_details[3]['index'])
+    #num = a.get_tensor(output_details[3]['index'])
   
     #Visualization of the results of a detection.
     vis_util.visualize_boxes_and_labels_on_image_array(
@@ -69,7 +101,7 @@ while(True):
             np.squeeze(boxes),
             np.squeeze(classes).astype(np.int32),
             np.squeeze(scores),
-            labels,
+            category_index,
             use_normalized_coordinates=True,
             line_thickness=2,
             min_score_thresh=0.20)
